be916cc9c150eb9b4e319cad93f029c597342bdf
kumaramardeep342 <kumaramardeep342@gmail.com>
2026-08-30 20:11:01 +0000

    began implementation

---
+++ Project details/RL_Project_Student_Package_2026/policy_file.py
@@ -0,0 +1,7 @@
+# Inside policy_0.py
+def run_policy(observation):
+    inv = observation["inventory"]
+    q1 = 20 if inv[0] < 50 else 0
+    q2 = 20 if inv[1] < 40 else 0
+    q3 = 10 if inv[2] < 30 else 0
+    return [q1, q2, q3]
